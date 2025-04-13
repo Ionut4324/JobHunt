d030a36b953bc153fd9af370504533278dac4608
Boaba Ionut <boabaionut21@stud.ase.ro>
2025-04-13 04:47:02 +0300

    Add everything

--- routes/jobsRoute.js
+++ routes/jobsRoute.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const Job = require("../models/jobModel")
 const User = require("../models/userModel")
 const moment = require("moment");
+const fs = require("fs");
+const path = require("path");
+
+
 router.get("/getalljobs", async (req, res) => {
 
     try {
@@ -31,6 +35,11 @@ router.post("/postjob", async (req, res) => {
 
 router.post("/editjob", async (req, res) => {
 
+    // const { user } = req.body
+
+    // const logFilePath = path.join(__dirname, "../logs/account.log");
+    // const logMessage = `{User ID: ${user._id}, First name: ${user.}, Date: ${moment().format('MMM DD YYYY')}}\n`;
+
     try {
         const updatedjob = await Job.findOneAndUpdate({ _id: req.body._id }, req.body)
         res.send('Job Updated Successfully')
@@ -45,6 +54,10 @@ router.post("/editjob", async (req, res) => {
 router.post("/applyjob", async (req, res) => {
 
     const { user, job } = req.body
+
+    const logFilePath = path.join(__dirname, "../logs/jobApplication.log");
+    const logMessage = `{ User ${user.username} with:\n\tUser ID: ${user._id} \n\temail: ${user.email} \n  Applied for: \n\tJob title: ${job.title} \n\tJob ID: ${job._id} \n\tDate: ${moment().utcOffset(-0).format("MMM DD YYYY, HH:mm:ss [UTC]")}\n}\n\n`;
+
     try {
 
         const jobDetails = await Job.findOne({ _id: job._id })
@@ -64,9 +77,21 @@ router.post("/applyjob", async (req, res) => {
 
         userDetails.appliedJobs.push(appliedJob)
         await userDetails.save()
+
+        // Append the log message to the log file
+        fs.appendFile(logFilePath, logMessage, (err) => {
+            if (err) {
+                console.error("Error writing to log file:", err);
+            } else {
+                console.log("Log entry added successfully.");
+            }
+        });
+
         res.send('Job Applied Successfully')
 
     } catch (error) {
+        const errorMessage = `{Error: ${error.message} \n\tDate: ${moment().utcOffset(-0).format("MMM DD YYYY, HH:mm:ss [UTC]")}}\n`;
+        fs.appendFileSync(logFilePath, errorMessage);
         res.send(error)
     }
 });
